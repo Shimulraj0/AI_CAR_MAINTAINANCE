2ddd97a3136b118bdb915b3d632125afbd4a4484
Shimulraj0 <shimulrajdas001@gmail.com>
2026-02-18 02:48:37 +0600

    refactor: Replace splash screen custom background painter with an image asset and simplify its content.

--- lib/views/splash/splash_view.dart
+++ lib/views/splash/splash_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../controllers/splash_controller.dart';
-import '../../widgets/painters/splash_background_painter.dart';
 
 class SplashView extends GetView<SplashController> {
   const SplashView({super.key});
@@ -16,10 +15,12 @@ class SplashView extends GetView<SplashController> {
         fit: StackFit.expand,
         children: [
           // Background Painter
-          // Background Painter
-          CustomPaint(
-            key: const Key('splash_background'),
-            painter: SplashBackgroundPainter(),
+          // Background Image
+          Image.asset(
+            'assets/images/splash_bg.png',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
           ),
 
           // Center Content
@@ -38,153 +39,8 @@ class SplashView extends GetView<SplashController> {
                     ),
                     // Using PNG asset
                     child: Image.asset(
-                      'assets/images/branded_logo.png',
+                      'assets/images/splash_logo.png',
                       fit: BoxFit.fill,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  // User Provided Structure (adapted)
-                  Container(
-                    width: 402,
-                    padding: const EdgeInsets.only(
-                      left: 26,
-                      right: 26,
-                      bottom: 14,
-                    ),
-                    clipBehavior: Clip.antiAlias,
-                    decoration: const BoxDecoration(),
-                    child: Stack(
-                      children: [
-                        SizedBox(
-                          width: double.infinity,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              const SizedBox(
-                                width: double.infinity,
-                                height: 30,
-                              ),
-                              SizedBox(
-                                width: 332,
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      width: 54,
-                                      height: 21,
-                                      clipBehavior: Clip.antiAlias,
-                                      decoration: ShapeDecoration(
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(
-                                            20,
-                                          ),
-                                        ),
-                                      ),
-                                      child: Stack(
-                                        children: [
-                                          Positioned(
-                                            left: 11,
-                                            top: 3,
-                                            child: SizedBox(
-                                              width: 33,
-                                              height: 15,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    // Row with spacing 4 manually handled
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        const SizedBox(width: 20, height: 14),
-                                        const SizedBox(width: 4),
-                                        const SizedBox(width: 16, height: 14),
-                                        const SizedBox(width: 4),
-                                        SizedBox(
-                                          width: 25,
-                                          height: 12,
-                                          child: Stack(
-                                            children: [
-                                              Positioned(
-                                                left: 2,
-                                                top: 2,
-                                                child: Container(
-                                                  width: 19,
-                                                  height: 8,
-                                                  decoration: ShapeDecoration(
-                                                    color: Colors.white,
-                                                    shape: RoundedRectangleBorder(
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                            1,
-                                                          ),
-                                                    ),
-                                                  ),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const Positioned(
-                          left: 26,
-                          top: 40,
-                          child: SizedBox(width: 6, height: 6),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 402,
-                    height: 40,
-                    clipBehavior: Clip.antiAlias,
-                    decoration: const BoxDecoration(),
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          left: 129.71,
-                          top: 24.25,
-                          child: SizedBox(
-                            width: 143.65,
-                            height: 6.88,
-                            child: Stack(
-                              children: [
-                                Positioned(
-                                  left: 0.29,
-                                  top: -0.25,
-                                  child: Container(
-                                    width: 144,
-                                    height: 6,
-                                    decoration: ShapeDecoration(
-                                      color: Colors.white,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
                     ),
                   ),
                 ],
